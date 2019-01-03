13838d8a0909962cfcdb6c2b68097605a59d7599
Gabriel Yan Mitoso <gymitoso@gmail.com>
2019-01-03 18:37:37 -0400

    Added google analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,21 +4,21 @@ jQuery(document).ready(function($) {
 
     $('.level-bar-inner').css('width', '0');
 
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-
-            var itemWidth = $(this).data('level');
-
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-
-        });
-
-    });
-
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
+
+});
+
+$(window).on('load', function() {
+
+    $('.level-bar-inner').each(function() {
+
+        var itemWidth = $(this).data('level');
+
+        $(this).animate({
+            width: itemWidth
+        }, 800);
+
+    });
 
 });
